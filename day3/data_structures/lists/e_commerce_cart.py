25efa40debed9f812c777b4d3b92f72d61b48992
Gardas Akash <akash39g@gmail.com>
2025-09-10 10:46:32 +0530

    Lists: E-commerce cart

---
+++ day3/data_structures/lists/e_commerce_cart.py
@@ -0,0 +1,35 @@
+class Cart:
+    def __init__(self):
+        self.products = []
+    
+    def add(self, item):
+        self.products.append(item)
+    
+    def remove(self, item):
+        self.products.remove(item)
+    
+    def search(self, item):
+        if item in self.products:
+            return True
+        else:
+            return False
+        
+    def size(self):
+        return len(self.products)
+    
+    def display(self):
+        print('-'*30)
+        print('YOUR CART')
+        print('-'*30)
+        print(f'Total Number of items: {self.size()}')
+        print(f'Items: ')
+        for item in self.products:
+            print(item, end=', ')
+
+
+items = ['Laptop', 'Headphones', 'Mouse', 'Keyboard', 'Mouse Pad', 'Laptop Stand']
+my_cart = Cart()
+for item in items:
+    my_cart.add(item)
+
+my_cart.display()
