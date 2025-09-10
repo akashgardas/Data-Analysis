c9ff7c4eb938c1dd1bce72cfd839f2c0191f3378
Gardas Akash <akash39g@gmail.com>
2025-09-10 11:45:40 +0530

    Tuples: School Students Records

--- day3/data_structures/tuples/school.py
+++ day3/data_structures/tuples/school.py
@@ -6,16 +6,16 @@ class School:
         self.students.append((rno, name, marks))
     
     def get_highest_scored_student(self):
-        top_std = max(self.students, key=lambda std: std[2])
+        top_std = max(self.students, key=lambda std: std[2])   
         return top_std
 
     def display(self):
         print('-'*30)
-        print('STUDENTS')
+        print('STUDENTS'.center(30, ' '))
         print('-'*30)
-        print('Roll No          Name        Marks')
+        print('Roll No\tName\t\tMarks')
         for std in self.students:
-            print(f'{std[0]}        {std[1]}        {std[2]}')
+            print(f'{std[0]}\t{std[1]}\t\t{std[2]}')
         
         print('-'*30)
         print(f'Highest marks student: {self.get_highest_scored_student()[1]}')
@@ -23,10 +23,10 @@ class School:
         
         print(f'Students with marks above 75')
         print('-'*30)
-        print('Roll No          Name        Marks')
+        print('Roll No\tName\t\tMarks')
         for std in self.students:
             if std[2] >= 75:
-                print(f'{std[0]}        {std[1]}        {std[2]}')
+                print(f'{std[0]}\t{std[1]}\t\t{std[2]}')
 
 school = School()
 school.add_student(1, 'John', 78)
@@ -36,7 +36,7 @@ school.add_student(4, 'Sophia', 72)
 school.add_student(5, 'Michael', 65)
 school.add_student(6, 'Olivia', 100)
 school.add_student(7, 'James', 91)
-school.add_student(8, 'Isabella', 79)
+school.add_student(8, 'Isabell', 79)
 school.add_student(9, 'Daniel', 83)
 school.add_student(10, 'Mia', 68)
 school.add_student(11, 'William', 99)
